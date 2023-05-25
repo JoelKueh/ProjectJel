47618b7f7baf51d1961b2097ec830271d9caca29
JCK-DESKTOP\joelk <joelckuehne@gmail.com>
2023-05-25 10:51:55 -0500

    Added build scripts for server

---
+++ build/build.bat
@@ -0,0 +1,2 @@
+cd %~dp0..\ProjectJel\datafiles\battle_sim_srv
+nexe index.js -o battle_sim_srv.exe -t windows --build
---
+++ build/init.bat
@@ -0,0 +1,3 @@
+npm install -g nexe
+cd %~dp0..\ProjectJel\datafiles\battle_sim_srv
+npm install
